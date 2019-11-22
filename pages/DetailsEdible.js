aba9d7bfc85d5b749dd8a1bc94edfdcf0ce62267
Petrica Nanca <petrica.nanca@softvision.ro>
2019-11-22 17:13:29 +0200

    Detail view slideshow

--- pages/DetailsEdible.js
+++ pages/DetailsEdible.js
@@ -2,19 +2,44 @@
 import React, {Component} from 'react';
 //import react in our code.
 import {StyleSheet, View, Button, Text} from 'react-native';
-// import all basic components
+import SliderShow from 'react-native-image-slider-show';
 
 export default class DetailsEdible extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [
+        require('../image/boletus.png'),
+        require('../image/boletus.png'),
+        require('../image/boletus.png'),
+        require('../image/boletus.png'),
+      ],
+    };
+  }
+
   static navigationOptions = ({navigation}) => ({
     title: navigation.state.params.name,
   });
   // Details non-edible
   render() {
     const {goBack} = this.props.navigation;
+    const { images } = this.props.navigation.state.params;
+    console.log(images);
     return (
       <View style={styles.MainContainer}>
-        <Text>{this.props.navigation.state.params.name}</Text>
-        <Button title="Go back" onPress={() => goBack()} />
+        <View style={{backgroundColor: 'blue', height: 200}}>
+          <SliderShow
+            dataSource={[
+              {url: require('../image/boletus.png')},
+              {url: require('../image/boletus.png')},
+              {url: require('../image/boletus.png')},
+            ]}
+          />
+        </View>
+        <View>
+          <Text>{this.props.navigation.state.params.name}</Text>
+          <Button title="Go back" onPress={() => goBack()} />
+        </View>
       </View>
     );
   }
@@ -23,9 +48,9 @@ export default class DetailsEdible extends Component {
 const styles = StyleSheet.create({
   MainContainer: {
     flex: 1,
-    paddingTop: 20,
+    paddingTop: 0,
     alignItems: 'center',
-    marginTop: 50,
-    justifyContent: 'center',
+    marginTop: 0,
+    justifyContent: 'flex-start',
   },
 });
