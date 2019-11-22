2d6f4ed1a45ee58a6da22f0a080604ead8565860
Petrica Nanca <petrica.nanca@softvision.ro>
2019-11-22 10:32:44 +0200

    Toggle search bar

--- pages/Search.js
+++ pages/Search.js
@@ -1,38 +1,52 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, TextInput} from 'react-native';
+import {StyleSheet, View, TextInput, TouchableOpacity} from 'react-native';
 import {Icon} from 'react-native-elements';
 
 export default class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {
+      text: '',
+      searchActive: false,
+    };
   }
+
+  toggleSearchBar = () => {
+    this.setState({ searchActive: !this.state.searchActive });
+  };
+
   render() {
+    const SearchBar = (
+      <View style={{width: 250, height: 50, backgroundColor: '#313218'}}>
+        <TextInput
+          style={{paddingTop: 18, color: 'white', fontSize: 20}}
+          placeholder="Caută ..."
+          placeholderTextColor={'white'}
+          onChangeText={text => this.setState({text})}
+          value={this.state.text}
+        />
+      </View>
+    );
+
     return (
       <View style={styles.MainContainer}>
         <View style={{flex: 1, flexDirection: 'row'}}>
-          <View style={{width: 250, height: 50, backgroundColor: '#313218'}}>
-            <TextInput
-              style={{paddingTop: 18, color: 'white' }}
-              placeholder="Caută ..."
-              placeholderTextColor={'white'} 
-              onChangeText={text => this.setState({text})}
-              value={this.state.text}
-            />
-          </View>
+          {this.state.searchActive && SearchBar}
           <View
             style={{
               width: 50,
               height: 50,
-              backgroundColor: 'steelblue',
+              backgroundColor: '#313218',
               paddingTop: 15,
             }}>
-            <Icon
-              name="ios-search"
-              type="ionicon"
-              color="#94C43D"
-              style={{paddingTop: 30, marginTop: 30}}
-            />
+            <TouchableOpacity onPress={() => this.toggleSearchBar()}>
+              <Icon
+                name="ios-search"
+                type="ionicon"
+                color="#94C43D"
+                style={{paddingTop: 30, marginTop: 30}}
+              />
+            </TouchableOpacity>
           </View>
         </View>
       </View>
