6ce6370e964c3dff7df0703fcd3f09c5ac21e2e9
Petrica Nanca <petrica.nanca@softvision.ro>
2019-11-28 16:53:28 +0200

    Added new images

--- image/Imap.js
+++ image/Imap.js
@@ -1,8 +1,10 @@
 const Imap = {
+    agaricus_bisporus_thumb: require('./agaricus_bisporus_thumb.jpg'),
     agaricus_bisporus_1: require('./agaricus_bisporus_1.jpg'),
     agaricus_bisporus_2: require('./agaricus_bisporus_2.jpg'),
     agaricus_bisporus_3: require('./agaricus_bisporus_3.jpg'),
     agaricus_bisporus_4: require('./agaricus_bisporus_4.jpg'),
+    agaricus_arvensis_thumb: require('./agaricus_arvensis_thumb.jpg'),
 };
 
 export default Imap;
